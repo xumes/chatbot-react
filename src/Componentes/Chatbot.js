1d397e545d8d2be1e8e7fbe1cd07ec050af8ffd5
Reginaldo Santos <reginaldosantos.br@gmail.com>
2018-08-16 16:53:20 -0300

    criando a estrutura dos componentes Header Footer Home e Chatbot

---
+++ src/Componentes/Chatbot.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Chatbot extends Component {
+    render() {
+        return (
+            <div>
+                Chatbot
+            </div>
+        )
+    }
+}
+
+export default Chatbot
