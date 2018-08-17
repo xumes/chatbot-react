4be34472ad2774b20930e756cc6a15f24142cdda
Reginaldo Santos <rmsantos@br.ibm.com>
2018-08-17 16:20:37 -0300

    componente Mensagem criado

--- src/Componentes/chat/ChatMensagem.js
+++ src/Componentes/chat/ChatMensagem.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { InputGroup, InputGroupAddon, Input, Button } from 'reactstrap'
 
 const ChatMensagem = props => {
     return (
         <div className='chat-mensagem'>
-            Digite sua mensagem
+            <InputGroup>
+                <Input placeholder='Digite sua mensagem' />
+                <InputGroupAddon addonType='append'>
+                    <Button color='success'>enviar</Button>
+                </InputGroupAddon>
+            </InputGroup>
         </div>
     )
 }
