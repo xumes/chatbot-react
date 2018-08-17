a78e09efe25e8371535d7593d217124dfd924f8e
Reginaldo Santos <reginaldosantos.br@gmail.com>
2018-08-17 16:25:45 -0300

    componente conversa

--- src/Componentes/chat/ChatConversa.js
+++ src/Componentes/chat/ChatConversa.js
@@ -1,10 +1,39 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { Alert, Badge } from 'reactstrap'
 
 class ChatConversa extends Component {
-    render(){
+    render() {
         return (
             <div className='chat-conversa'>
-                Olá, como posso lhe ajudar hoje?
+
+                <Badge color='primary'>
+                    você disse:
+                </Badge>
+                <Alert color='primary'>
+                    Olá, como posso lhe ajudar hoje?
+                </Alert>
+
+                <Badge color='warning'>
+                    chatbot disse:
+                </Badge>
+                <Alert color='warning'>
+                    Olá, como posso lhe ajudar hoje?
+                </Alert>
+
+                <Badge color='primary'>
+                    você disse:
+                </Badge>
+                <Alert color='primary'>
+                    Olá, como posso lhe ajudar hoje?
+                </Alert>
+
+                <Badge color='warning'>
+                    chatbot disse:
+                </Badge>
+                <Alert color='warning'>
+                    Olá, como posso lhe ajudar hoje?
+                </Alert>
+
             </div>
         )
     }
