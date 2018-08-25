89b50cbdae402e7e76cc532ffc3c3efe83bcb4b7
Reginaldo Santos <reginaldosantos.br@gmail.com>
2018-08-25 01:36:33 -0300

    mapStateToProps

--- src/Componentes/chat/ChatConversa.js
+++ src/Componentes/chat/ChatConversa.js
@@ -1,42 +1,46 @@
 import React, { Component } from 'react'
 import { Alert, Badge } from 'reactstrap'
+import { connect } from 'react-redux'
 
 class ChatConversa extends Component {
+
+    renderMensagem(msg, autor) {
+        return (
+            <div>
+                {
+                    autor == 'user' && <span>
+                        <Badge color='primary'>você disse:</Badge>
+                        <Alert color='primary'>{msg}</Alert>
+                    </span>
+                }
+
+                {
+                    autor == 'bot' && <span>
+                        <Badge color='warning'>chatbot disse:</Badge>
+                        <Alert color='warning'>{msg}</Alert>
+                    </span>
+                }
+            </div>
+        )
+    }
+
     render() {
         return (
             <div className='chat-conversa'>
-
-                <Badge color='primary'>
-                    você disse:
-                </Badge>
-                <Alert color='primary'>
-                    Olá, como posso lhe ajudar hoje?
-                </Alert>
-
-                <Badge color='warning'>
-                    chatbot disse:
-                </Badge>
-                <Alert color='warning'>
-                    Olá, como posso lhe ajudar hoje?
-                </Alert>
-
-                <Badge color='primary'>
-                    você disse:
-                </Badge>
-                <Alert color='primary'>
-                    Olá, como posso lhe ajudar hoje?
-                </Alert>
-
-                <Badge color='warning'>
-                    chatbot disse:
-                </Badge>
-                <Alert color='warning'>
-                    Olá, como posso lhe ajudar hoje?
-                </Alert>
-
+                {
+                    this.props.mensagens.map(key => {
+                        return this.renderMensagem(key, 'user')
+                    })
+                }
             </div>
         )
     }
 }
 
-export default ChatConversa
+const mapStateToProps = (state) => {
+    return {
+        mensagens: state.chat.mensagens
+    }
+}
+
+export default connect(mapStateToProps, null)(ChatConversa)
