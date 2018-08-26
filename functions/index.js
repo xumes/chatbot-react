eaeb5ed0a70c2546018061e9851edbd5b4fca851
Reginaldo Santos <rmsantos@br.ibm.com>
2018-08-26 00:11:50 -0300

    Criando a API que conversa com o Watson

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,39 @@
 const functions = require('firebase-functions');
+const watson = require('watson-developer-cloud/assistant/v1')
+require('dotenv').config()
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
 // exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+//  response.send("Hello from Chatbot!");
 // });
+
+const chatbot = new watson({
+    username: process.env.USERNAME,
+    password: process.env.PASSW0RD,
+    version: process.env.VERSION,
+});
+
+const workspace_id = process.env.WORKSPACE_ID;
+
+exports.conversa = functions.https.onRequest((req, res) => {
+    let payload = {
+        workspace_id,
+        context: req.body.context || {},
+        input: req.body.input || {}
+    };
+
+    chatbot.message(payload, (err, data) => {
+        if (err) {
+            return res.status(err.code || 500).json(err);
+        }
+
+        return res.json(trataResposta(payload, data));
+    })
+})
+
+const trataResposta = (payload, resposta) => {
+    console.log('watson disse: ', resposta);
+    return resposta;
+}
