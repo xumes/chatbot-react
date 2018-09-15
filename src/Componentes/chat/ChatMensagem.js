642b917d3676c94cb623379c3dd8faefcc248243
Reginaldo Santos <rmsantos@br.ibm.com>
2018-09-15 20:20:50 -0300

    middleware

--- src/Componentes/chat/ChatMensagem.js
+++ src/Componentes/chat/ChatMensagem.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import { InputGroup, InputGroupAddon, Input, Button } from 'reactstrap'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
-import {enviaMensagem} from './../../store/actions/chat'
+import { enviaMensagem } from './../../store/actions/chat'
+import { conversaWatson } from './../../store/actions/watson'
 
 class ChatMensagem extends Component {
     constructor(props) {
@@ -11,7 +12,7 @@ class ChatMensagem extends Component {
         this.inputEnviaTexto = this.inputEnviaTexto.bind(this)
     }
 
-    inputEnviaTexto(e){
+    inputEnviaTexto(e) {
         if (e.keyCode === 13) {
             console.log(e.target.value)
             const mensagem = {
@@ -19,6 +20,7 @@ class ChatMensagem extends Component {
                 origem: 'user'
             }
             this.props.enviaTexto(mensagem)
+            this.props.conversaWatson(mensagem)
             e.target.value = ''
         }
     }
@@ -40,7 +42,8 @@ class ChatMensagem extends Component {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        enviaTexto: (msg) => dispatch(enviaMensagem(msg))
+        enviaTexto: (msg) => dispatch(enviaMensagem(msg)),
+        conversaWatson: (msg) => dispatch(conversaWatson(msg, ''))
     }
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
-import {createStore} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
 
 import reducers from './reducers'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const middlewares = [thunk]
+
 const store = createStore(
     reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(
+        applyMiddleware(...middlewares)
+    )
 )
 
 export default store
